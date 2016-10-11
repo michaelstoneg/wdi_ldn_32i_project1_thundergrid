812394647a29e657d1f91c032df8e021a4b2599e
michael grandison <michael.grandison@gmail.com>
2016-10-11 16:11:26 +0100

    cleaned up ui & fixed multi-weapon bug

--- js/app.js
+++ js/app.js
@@ -4,14 +4,18 @@
 
 console.log("It's Aliiive!!!!");
 
+var boardSize = 36;
+var boardFrame = document.getElementsByClassName('boardFrame');
 var gridSquares = document.getElementsByClassName('gridSquare');
 var messageBox = document.getElementById('messageBox');
-var infoBoxOne = document.getElementById('playerOne');
-var infoBoxTwo = document.getElementById('playerTwo');
+var infoBoxOne = document.getElementById('playerOneInfo');
+var infoBoxTwo = document.getElementById('playerTwoInfo');
 var playerOneItems = document.getElementById('playerOneItems');
 var playerTwoItems = document.getElementById('playerTwoItems');
 var target = 0;
-var spearPosition = undefined;
+var spearLocation = undefined;
+var bowLocation = undefined;
+var magicStaffLocation = undefined;
 var spearImage = document.getElementById('spearImage');
 
 playerOne = {
@@ -75,31 +79,33 @@ var tauntSound = document.getElementById('taunt');
 var spawnSound = document.getElementById('spawn');
 
 
+// for (var a = 0; a < boardSize; a ++) {
+//    console.log(boardSize);
+//    var gridSquares = document.createElement('div');
+//    gridSquares.setAttribute('className', 'gridSquares');
+//    console.log(gridSquares);
+//    console.log(boardFrame);
+//    boardFrame.appendChild(gridSquares);
+// }
+
+
 for (var i = 0; i < gridSquares.length; i ++) {
   gridSquares[i].setAttribute('data-position', i);
-  console.log("mapping grid coordinates", gridSquares[i].getAttribute('data-position'));
 }
-// bgm.volume = .5;
-// bgm.play();
+
+bgm.volume = 0.5;
+bgm.play();
 function  spawnPlayer() {
   // choose random spawn point
   if (player.position === undefined) {
-    console.log("spawning player");
     var spawnPoint = gridSquares[Math.floor(Math.random() * 30)];
-    console.log("spawing at", spawnPoint.getAttribute('data-position'));
-    console.log("Your direction is", player.direction);
     spawnSound.play();
     spawnPoint.innerHTML = player.avatar;
     player.position = parseFloat(spawnPoint.getAttribute('data-position'));
-    console.log("player.position is", player.position);
-    player.infoBox.innerHTML = player.name + "   spawned at   " + player.position;
     player.weapon = 'sword';
     player.health = 'alive';
     player.action = 'move';
-  } else {
-    console.log("No double spawnsies");
-    player.infoBox.innerHTML = "No Double Spawnzies";
-  }
+  } 
 }
 
 window.addEventListener("keyup", function(e) {
@@ -115,12 +121,10 @@ window.addEventListener("keyup", function(e) {
   }
   if (whatPress === 'c' || whatPress === 'f') {
     player = playerOne;
-    console.log("player is", player);
     fightClub();
   }
   if (whatPress === 'n' || whatPress === 'h') {
   player = playerTwo;
-  console.log("player is", player);
   fightClub();
   }
   else {
@@ -129,163 +133,99 @@ window.addEventListener("keyup", function(e) {
 });
 
 function hotStepper () {
+
   if (player.health === 'dead') {
-  console.log('where you think you going? you dead. respawn');
-  player.infoBox.innerHTML = 'where you think you going?  ', + player.name + ' you dead. respawn';
-}
+  console.log('respawn');
+  }
+
   if (whatPress === 'w' || whatPress === 'a' || whatPress === 's' || whatPress === 'd') {
     player = playerOne;
-    console.log("player is now", player);
-    console.log("direction", player.direction);
   }
   if (whatPress === 'i' || whatPress === 'j' || whatPress === 'k' || whatPress === 'l') {
     player = playerTwo;
-    console.log("player is now", player);
-    console.log("direction", player.direction);
   }
   if (whatPress === player.direction) {
-  console.log("ok to move");
   player.action = 'move';
   targeting();
+
   if (target < 0 || target > 35) {
-  gridSquares[player.position].innerHTML = '';
-  deathSound.playbackRate = 2;
-  deathSound.play();
-  player.health = 'dead';
-  player.position = undefined;
-  player.weapon = '';
-  player.action = '';
-  console.log("You just ran off a cliff");
-  player.infoBox.innerHTML = player.name + 'You just ran off a cliff';
-}
+    gridSquares[player.position].innerHTML = '';
+    deathSound.playbackRate = 2;
+    deathSound.play();
+    player.health = 'dead';
+    player.position = undefined;
+    player.weapon = '';
+    player.action = '';
+  }
   if (target !== playerOne.position && target !== playerTwo.position) {
-    console.log("target accquired. Redy to move to", target);
     player.action = 'move';
     jumpSound.playbackRate = 2;
     jumpSound.play();
     gridSquares[player.position].innerHTML = '';
     gridSquares[player.position].style.background = '';
     gridSquares[target].innerHTML = player.avatar;
-    messageBox.innerHTML = player.name + "   moved to   " + target;
     player.position = target;
-    console.log("direction", player.direction);
+    treasureHunt(player.position);
   }
-    if (player.position === parseFloat(spearLocation.getAttribute('data-position'))) {
-      player.weapon = 'spear';
-      console.log("A wild spear appeared");
-      console.log(player.name + "   now has a   " + player.weapon);
-      player.bag.innerHTML = '<img src="images/spear.png">';
-      powerUpSound.playbackRate = 1.5;
-      powerUpSound.play();
-      player.infoBox.innerHTML = player.name + "   has a   " + player.weapon;
-      altPlayer.infoBox.innerHTML = player.name + "   has a   " + player.weapon;
-      spearLocation = undefined;
-      // gridSquares[player.position].innerHTML = '';
-    }
-    if (player.position === parseFloat(bowLocation.getAttribute('data-position'))) {
-      player.weapon = 'bow';
-      console.log("A wild bow appeared");
-      console.log(player.name + "   now has a   " + player.weapon);
-      player.bag.innerHTML = '<img src="images/bow.png">';
-      powerUpSound.playbackRate = 1.5;
-      powerUpSound.play();
-      player.infoBox.innerHTML = player.name + "   has a   " + player.weapon;
-      altPlayer.infoBox.innerHTML = player.name + "   has a   " + player.weapon;
-      bowLocation = undefined;
-    }
- else { console.log("nope");
- player.action = '';
+
 }
-}
-  if (whatPress !== player.direction) {
-    console.log("turn around");
+  else {
     imageRotator ();
     }
   }
 
   function targeting() {
+    if (player.weapon === 'magicStaff' && player.action === 'attack') {
+      target = altPlayer.position;
+    }
     if (player.direction === 'd' || player.direction === 'l' ) {
       if (player.action === 'attack' && player.weapon === 'spear') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
-        // gridSquares[player.position + 1].innerHTML = '<img src="images/spear.png">';
-        // console.log("spear img goes here", gridSquares[player.position + 1]);
         target = player.position + 2;
-        console.log('target is', target);
       }
       if (player.action === 'attack' && player.weapon === 'bow') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
-        // gridSquares[player.position + 1].innerHTML = '<img src="images/spear.png">';
-        // console.log("spear img goes here", gridSquares[player.position + 1]);
         target = player.position + 4;
-        console.log('target is', target);
       }
       if (player.action === 'move' || player.weapon === 'sword') {
-        console.log("good for you buddy");
         target = player.position + 1;
-        console.log('target is', target);
       }
     }
-    if (player.direction === 's' || player.direction === 'k' ) {
-        if (player.action === 'attack' && player.weapon === 'spear') {
-          console.log(player.name, '   used  ', player.weapon, player.action );
-          target = player.position + 12;
-          console.log('target is', target);
-        }
-        if (player.action === 'attack' && player.weapon === 'bow') {
-          console.log(player.name, '   used  ', player.weapon, player.action );
-          // gridSquares[player.position + 1].innerHTML = '<img src="images/spear.png">';
-          // console.log("spear img goes here", gridSquares[player.position + 1]);
-          target = player.position + 24;
-          console.log('target is', target);
-        }
-        if (player.action === 'move' || player.weapon === 'sword') {
-          console.log("good for you buddy");
+  if (player.direction === 's' || player.direction === 'k' ) {
+      if (player.action === 'attack' && player.weapon === 'spear') {
+        target = player.position + 12;
+      }
+      if (player.action === 'attack' && player.weapon === 'bow') {
+        target = player.position + 24;
+      }
+      if (player.action === 'move' || player.weapon === 'sword') {
           target = player.position + 6;
-          console.log('target is', target);
-        }
+      }
   }
   if (player.direction === 'a' || player.direction === 'j') {
       if (player.action === 'attack' && player.weapon === 'spear') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
         target = player.position - 2;
-        console.log('target is', target);
       }
       if (player.action === 'attack' && player.weapon === 'bow') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
-        // gridSquares[player.position + 1].innerHTML = '<img src="images/spear.png">';
-        // console.log("spear img goes here", gridSquares[player.position + 1]);
         target = player.position - 4;
-        console.log('target is', target);
       }
       if (player.action === 'move' || player.weapon === 'sword') {
-        console.log("good for you buddy");
         target = player.position - 1;
-        console.log('target is', target);
       }
-  }
+    }
   if (player.direction === 'w' || player.direction === 'i') {
       if (player.action === 'attack' && player.weapon === 'spear') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
         target = player.position - 12;
-        console.log('target is', target);
       }
       if (player.action === 'attack' && player.weapon === 'bow') {
-        console.log(player.name, '   used  ', player.weapon, player.action );
-        // gridSquares[player.position + 1].innerHTML = '<img src="images/spear.png">';
-        // console.log("spear img goes here", gridSquares[player.position + 1]);
         target = player.position - 24;
-        console.log('target is', target);
       }
       if (player.action === 'move' || player.weapon === 'sword') {
-        console.log("good for you buddy");
         target = player.position - 6;
-        console.log('target is', target);
       }
+    }
   }
-}
+
 
 function imageRotator() {
-    console.log("this is imageRotator");
     if (whatPress === 'i' || whatPress === 'w') {
       player.direction = whatPress;
       player.avatar = player.up ;
@@ -308,77 +248,99 @@ function fightClub () {
   } if (player === playerTwo) {
     altPlayer = playerOne;
   }
+
   if (whatPress === 'c' || whatPress === 'n') {
     player.action = 'attack';
     attackSound.playbackRate = 2.5;
     attackSound.play();
-    console.log(player, "drops his defense");
-    console.log(player, "attacks");
     gridSquares[player.position].style.background = '#ff5050';
     targeting();
-    console.log("target accquired at", target);
-    console.log("target is ", target);
-    // animator();
       if (target === altPlayer.position) {
-        console.log("attacking", altPlayer);
-        player.infoBox.innerHTML = player.name + "   attacks   " + altPlayer.name;
         if (altPlayer.action !== 'defend' && altPlayer.health !== 'dead' ) {
-        deathSound.playbackRate = 1.5;
-        deathSound.play();
-        altPlayer.health = 'dead';
-        console.log("kill confirmed");
-        player.action = '';
-        player.infoBox.innerHTML = '';
-        altPlayer.infoBox.innerHTML = altPlayer.name + "   is dead";
-        player.infoBox.innerHTML = altPlayer.name + "   is dead";
-        gridSquares[altPlayer.position].innerHTML = '<img src="images/blood.png">';
-        gridSquares[altPlayer.position].style.background = '';
-        altPlayer.position = undefined;
-        player.score = player.score + 1;
-        // tauntSound.playbackRate = .7;
-        // tauntSound.play();
-        console.log('player score', player.score);
-        player.infoBox.innerHTML = player.name + "'s score is" + player.score;
-        if (player.score === 1) {
-          supplyDrop();
+          deathSound.playbackRate = 1.5;
+          deathSound.play();
+          altPlayer.health = 'dead';
+          player.action = '';
+          altPlayer.bag.innerHTML = '';
+          gridSquares[altPlayer.position].innerHTML = '<img src="images/blood.png">';
+          gridSquares[altPlayer.position].style.background = '';
+          altPlayer.position = undefined;
+          player.score = player.score + 1;
+          player.infoBox.innerHTML = player.name + "'s score is   " + player.score + '<br>';
+          supplyDrop(player.score);
         }
-      } else {
-        console.log(altPlayer, "defended");
-        player.infoBox.innerHTML = altPlayer.name + "   defended";
-        altPlayer.infoBox.innerHTML = altPlayer.name + "   defended";
-        defendedSound.playbackRate = 1;
-        defendedSound.play();
-      }
+        else {
+          defendedSound.playbackRate = 1;
+          defendedSound.play();
+        }
     }
       else {
         console.log("no one's home");
       }
-    } if (whatPress === 'f' || whatPress === 'h') {
+    }
+    if (whatPress === 'f' || whatPress === 'h') {
       player.action = 'defend';
-      console.log(player, player.action);
       defendSound.playbackRate = 1;
       defendSound.play();
-      player.infoBox.innerHTML = player.name + "   defends";
-      altPlayer.infoBox.innerHTML = player.name + "   defends";
       gridSquares[player.position].style.background = '#3399ff';
   }
 }
 
-function supplyDrop() {
+function supplyDrop(ps) {
 
-  spearLocation = gridSquares[Math.floor(Math.random() * 30)];
-  if (spearLocation !== playerOne.location || spearLocation !== playerTwo.location) {
-  console.log("Spears go here   ", spearLocation);
-  spearLocation.innerHTML = '<img src="images/spear.png">';
-  supplyDropSound.playbackRate = 2;
-  supplyDropSound.play();
-}
-  bowLocation = gridSquares[Math.floor(Math.random() * 30)];
-  if (bowLocation !== playerOne.location || bowLocation !== playerTwo.location) {
-  console.log("bow goes here   ", bowLocation);
-  bowLocation.innerHTML = '<img src="images/bow.png">';
-  supplyDropSound.playbackRate = 2;
-  supplyDropSound.play();
+  if (ps === 1) {
+    spearLocation = gridSquares[Math.floor(Math.random() * 30)];
+    if (spearLocation.innerHTML === '') {
+      spearLocation.innerHTML = '<img src="images/spear.png">';
+      supplyDropSound.playbackRate = 2;
+      supplyDropSound.play();
+    }
+  }
+  else if (ps === 2) {
+    bowLocation = gridSquares[Math.floor(Math.random() * 30)];
+      if (bowLocation.innerHTML === '') {
+        bowLocation.innerHTML = '<img src="images/bow.png">';
+        supplyDropSound.playbackRate = 2;
+        supplyDropSound.play();
+      }
+    }
+  else if (ps === 3) {
+    magicStaffLocation = gridSquares[Math.floor(Math.random() * 30)];
+    if (magicStaffLocation.innerHTML === '') {
+      magicStaffLocation.innerHTML = '<img src="images/magicStaff.png">';
+      supplyDropSound.playbackRate = 2;
+      supplyDropSound.play();
+    }
+  }
+
 }
 
+
+function treasureHunt() {
+  console.log(bowLocation);
+  console.log(gridSquares[player.position]);
+  if (gridSquares[player.position] === spearLocation) {
+    player.weapon = 'spear';
+    player.bag.innerHTML = '<img src="images/spear.png">';
+    powerUpSound.playbackRate = 1.5;
+    powerUpSound.play();
+    spearLocation = undefined;
+  }
+
+  if (gridSquares[player.position] === magicStaffLocation) {
+    console.log('!!!!!!!!!!');
+    player.weapon = 'magicStaff';
+    player.bag.innerHTML = '<img src="images/magicStaff.png">';
+    powerUpSound.playbackRate = 1.5;
+    powerUpSound.play();
+    magicStaffLocation = undefined;
+  }
+
+  if (gridSquares[player.position] === bowLocation) {
+    player.weapon = 'bow';
+    player.bag.innerHTML = '<img src="images/bow.png">';
+    powerUpSound.playbackRate = 1.5;
+    powerUpSound.play();
+    bowLocation = undefined;
+  }
 }
